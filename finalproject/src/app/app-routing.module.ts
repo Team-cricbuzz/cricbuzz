6df839607022e6408c236a6d70e2790eb261732f
Kohli <simerpreet.kohli@capgemini.com>
2019-01-16 11:12:13 +0530

    static_home_page

--- finalproject/src/app/app-routing.module.ts
+++ finalproject/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "" , component : ComponentsComponent}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
