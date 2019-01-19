fec127fc27dfc61704fbbcb4129f246b55be4842
Team-cricbuzz <parthibansankar@ymail.com>
2019-01-19 15:12:26 +0530

    commit 11

--- cricbuzz-parthi/cricbuzz/src/app/matches/matches.service.ts
+++ cricbuzz-parthi/cricbuzz/src/app/matches/matches.service.ts
@@ -2,16 +2,23 @@ import {Injectable} from "@angular/core";
 import { HttpClient,HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IMatches } from '../imatches';
-import { Response } from 'selenium-webdriver/http';
+import { match } from './match';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
 providedIn:  'root'
 })
 
 export  class  MatchesService {
-
     private  matches:IMatches[];
-    API_URL  =  'https://cricapi.com/api/matches?apikey=SBXNkRA46WMvllmPx3FAqKibKQu2';
+    private extractData(res: Response) {
+        let body = res;
+        return body || { };
+      }
 constructor(private  http:  HttpClient) {}
+list(){
+    const url = 'https://cricapi.com/api/matches?apikey=SBXNkRA46WMvllmPx3FAqKibKQu2';
+    return this.http.get<match[]>(url);
 
+}
 }
